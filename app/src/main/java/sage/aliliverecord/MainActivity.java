74c13c401681aca3e20667a6d54b8b14ae6d23ff
Sage <salmanit@163.com>
2017-09-22 15:33:04 +0800

    2017-08-16

--- app/src/main/java/sage/aliliverecord/MainActivity.java
+++ app/src/main/java/sage/aliliverecord/MainActivity.java
@@ -1,10 +1,18 @@
 package sage.aliliverecord;
 
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.ImageView;
+
+import com.alibaba.livecloud.model.AlivcWatermark;
+
+import java.io.File;
 
 import sage.libaliliverecord.record.ErrorResult;
 import sage.libaliliverecord.record.SurfaceViewAli;
@@ -12,7 +20,7 @@ import sage.libaliliverecord.record.SurfaceViewAli;
 public class MainActivity extends AppCompatActivity {
 
     String url="http://tianqi.2345.com/theme2/img/logo160722.png";
-    String pushUrl="tmp://video-center.alivecdn.com/temp/test1466295255657-65e172e6-1b96-4660-9f2f-1aba576d84e8?vhost=http://livecdn.video.taobao.com";
+    String pushUrl="rtmp://video-center.readymade.cn/readymade/liveSteam4?vhost=live.readymade.cn&auth_key=1504146202-0-0-cf6e6d45508e2c0d16de5bcd867e512a";//这个地址自己写。
     SurfaceViewAli surfaceViewAli;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,9 +28,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
          surfaceViewAli= (SurfaceViewAli) findViewById(R.id.sfv_ali);
         surfaceViewAli.setPushUrl(pushUrl);
-        surfaceViewAli.changeWaterMark(url,20,20, SurfaceViewAli.WaterMarkPosition.BOTTOM_RIGHT);
 
-        surfaceViewAli.startPreview();
+        url=new File(Environment.getExternalStorageDirectory(),"apk/water.png").getAbsolutePath();
+        System.out.println("url==="+url);
+        ImageView iv= (ImageView) findViewById(R.id.iv_logo);
+        iv.setImageBitmap(BitmapFactory.decodeFile(url));
+        //水印图片不知道咋回事，貌似只有assets下的有效
+        surfaceViewAli.changeWaterMark("assets://Alivc/wartermark/logo.png",20,80, AlivcWatermark.SITE_BOTTOM_RIGHT);
+
+//        surfaceViewAli.startPreview();
 
 
         surfaceViewAli.setErrorResult(new ErrorResult() {
@@ -43,6 +57,7 @@ public class MainActivity extends AppCompatActivity {
         CheckBox cb_flash= (CheckBox) findViewById(R.id.cb_flash);
         CheckBox cb_mute= (CheckBox) findViewById(R.id.cb_mute);
         CheckBox cb_camera= (CheckBox) findViewById(R.id.cb_camera);
+        CheckBox cb_orientation= (CheckBox) findViewById(R.id.cb_orientation);
 
         cb_beauty.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
@@ -66,6 +81,12 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 surfaceViewAli.changeCamera();
+            }
+        });
+        cb_orientation.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                surfaceViewAli.changeOrientation(isChecked);
             }
         });
     }
